21feadfa59a6c382b84d26b96b83f9ebd342b309
pramesha-m <161158811+pramesha-m@users.noreply.github.com>
2025-04-16 23:58:45 -0400

    Update __init__.py

--- App/models/__init__.py
+++ App/models/__init__.py
@@ -1,1 +1,6 @@
-from .user import *
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+
+from .user import User            
+from .ingredient import Ingredient
+from .recipe import Recipe, recipe_ingredients
